6bc0451b931bf548a7a641f63317df58ae63cb36
liuyang <2522680278@qq.com>
2022-11-17 19:01:16 +0800

    引入router

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,12 +1,22 @@
-import { ReportHandler } from 'web-vitals';
+import {
+  ReportHandler,
+} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({
+      getCLS, getFID, getFCP, getLCP, getTTFB,
+    }) => {
+      // CLS (Cumulative Layout Shift) :累计布局偏移，得分范围0-1，指的是网页布局在加载期间的偏移量
+      // 0表示没有偏移，1表示最大偏移
       getCLS(onPerfEntry);
+      // FID (First Input Delay):首次输入延迟。
       getFID(onPerfEntry);
+      // FCP(First Contentful Paint)首次内容绘制。
       getFCP(onPerfEntry);
+      // LCP (Largest Contentful Paint)：最大内容渲染时间。
       getLCP(onPerfEntry);
+      // TTFB (Time to First Byte) 首字节到达的时间点。
       getTTFB(onPerfEntry);
     });
   }
---
+++ src/views/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@/assets/scss/home.scss';
+
+function Home() {
+  const one = 111;
+  return (
+    <div className="home">
+      <div>{one}</div>
+    </div>
+  );
+}
+
+export default Home;
